9f0e9a8650ce8575ae5b404822f31dd8056a4493
madjonga <madjonga123@gmail.com>
2023-04-12 23:09:27 +0300

    Make 4x4 grid

--- javascript.js
+++ javascript.js
@@ -1,2 +1,12 @@
 const container = document.querySelector('#container')
 
+for (i = 0; i < 4; i++){
+    const column = document.createElement('div')
+    column.classList.add('column')
+    container.appendChild(column)
+    for (j = 0; j < 4; j++) {
+        const row = document.createElement('div')
+        row.classList.add('row')
+        column.appendChild(row)
+    }
+}
