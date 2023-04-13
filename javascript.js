305ec1a09769c83ed351dc2634aba29bb86602be
madjonga <madjonga123@gmail.com>
2023-04-13 15:30:37 +0300

    Set a hover effect

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 const container = document.querySelector('#container')
 
-for (i = 0; i < 4; i++){
+const size = 100
+for (i = 0; i < size; i++){
     const column = document.createElement('div')
     column.classList.add('column')
     container.appendChild(column)
-    for (j = 0; j < 4; j++) {
+    for (j = 0; j < size; j++) {
         const row = document.createElement('div')
         row.classList.add('row')
+        row.classList.add('cell')
         column.appendChild(row)
     }
 }
+
+let cells = document.querySelectorAll('.cell')
+cells.forEach(cell => cell.addEventListener('mouseover', function(e) {
+    this.style.backgroundColor = 'black'
+}))
